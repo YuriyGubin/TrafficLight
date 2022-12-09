6df1b5d16a3964f36262324601e2696d4ca38498
GubinYuriy <iosdevelop.2022@gmail.com>
2022-12-09 09:04:53 +0500

    Working code

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -7,41 +7,74 @@
 
 import SwiftUI
 
+enum LightColor {
+    case red
+    case yellow
+    case green
+}
+
 struct ContentView: View {
     
-    @State private var tapCounter = 0
+    @State private var buttonTitle = "START"
+    
+    @State private var redOpacity      = 0.2
+    @State private var yellowOpacity   = 0.2
+    @State private var greenOpacity    = 0.2
+    
+    @State private var lightColor = LightColor.red
+    
+    private let isOn    = 1.0
+    private let isOff   = 0.2
+    
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
+            
             VStack {
                 Spacer()
-                LightView(color: .red, opacity: 0.5)
-                LightView(color: .yellow, opacity: 0.5)
-                LightView(color: .green, opacity: 0.5)
+                
+                LightView(color: .red, opacity: redOpacity)
+                LightView(color: .yellow, opacity: yellowOpacity)
+                LightView(color: .green, opacity: greenOpacity)
+                
                 Spacer()
+                
                 Button(action: switchColor) {
-                    Text("START")
+                    Text(buttonTitle)
                         .foregroundColor(.white)
                         .font(.largeTitle)
-                        .padding(EdgeInsets(
-                            top: 16,
-                            leading: 32,
-                            bottom: 16,
-                            trailing: 32
-                        ))
-                        .background(Color.blue)
-                        .clipShape(Capsule())
-                        .overlay(Capsule().stroke(Color.white, lineWidth: 3))
                 }
+                .frame(width: 140, height: 40)
+                .padding()
+                .background(Color.blue)
+                .clipShape(Capsule())
+                .overlay(Capsule().stroke(Color.white, lineWidth: 3))
+                
                 Spacer()
             }
         }
     }
     
     func switchColor() {
+        if buttonTitle == "START" {
+            buttonTitle = "NEXT"
+        }
         
-        
+        switch lightColor {
+        case .red:
+            redOpacity = isOn
+            greenOpacity = isOff
+            lightColor = .yellow
+        case .yellow:
+            yellowOpacity = isOn
+            redOpacity = isOff
+            lightColor = .green
+        case .green:
+            greenOpacity = isOn
+            yellowOpacity = isOff
+            lightColor = .red
+        }
         
     }
 }
